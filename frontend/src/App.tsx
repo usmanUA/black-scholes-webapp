2522895e2a3b607de1cc8c726a8d9be89ee051ba
Ahmed <adusman@lm1-500-25454.local>
2025-11-01 15:14:48 +0200

    Add results route in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import './App.css'
 import Home from "./pages/home";
+import Results from "./pages/results";
 
 function App() {
 
 	return (
 		<Routes>
 			<Route path="/" element={<Home />} />
+			<Route path="/results" element={<Results />} />
 		</Routes>
 	);
 }
